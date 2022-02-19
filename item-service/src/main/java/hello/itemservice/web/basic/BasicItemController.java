d531f045d0a393b7f918bafef8de98b43ed0eb67
jeonghaemin <jhm0828@gmail.com>
2022-02-19 21:57:15 +0900

    상품 관리 예제 - 상품 등록 처리

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,9 +36,38 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    /**
+     * @RequestParam 사용
+     */
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model
+    ) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 사용
+     * - Item 객체를 생성하고, Item 객체에 요청 파라미터의 값들을 프로퍼티 접근법(setter) 를 통해 입력해준다.
+     * - @ModelAttribute(name) 은 자동으로 모델에 추가되며 모델에 추가될 때 키 값은 name
+     * - name 속성을 생략시 디폴트 값은 클래스 이름의 첫글자를 소문자로 바꾼 값으로. HelloData -> helloData
+     * - @ModelAttribute 생략 가능
+     */
     @PostMapping("/add")
-    public String save() {
-        return "xxx";
+    public String addItemV2(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
